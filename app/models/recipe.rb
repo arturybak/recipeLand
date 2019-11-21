b24d625f53b3fffa20243ab3b5bd51855156edcc
Artur <ar01094@surrey.ac.uk>
2019-11-21 13:27:55 +0000

    Added image validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,15 @@
 class Recipe < ApplicationRecord
   has_one_attached :image
-  validates_presence_of :title, :description, :image
+  validates :title, :description, presence: true
+  validate :correct_image_attachment
+
+  private
+  def correct_image_attachment
+    if !image.attached?
+      errors.add(:image, 'No image uploaded')
+    elsif !image.image?
+      image.purge #delete uploaded file
+      errors.add(:image, 'File uploaded must be an image')
+    end
+  end
 end
